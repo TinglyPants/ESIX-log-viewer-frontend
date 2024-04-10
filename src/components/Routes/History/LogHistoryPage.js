4c0a0bb52df38c19734c73798597a72e6290a90b
TinglyPants <chalkytangfastics@gmail.com>
2024-04-10 22:01:17 +0100

    Very basic log viewing is up and running.

--- src/components/Routes/History/LogHistoryPage.js
+++ src/components/Routes/History/LogHistoryPage.js
@@ -1,31 +1,38 @@
-import LogHistoryElement from "./LogHistoryElement";
+import { useState } from "react";
 import LogHistoryViewer from "./LogHistoryViewer";
 
 export default function LogHistoryPage() {
+    // Todo: Get it to generate the dates of the last x (1-10) days. Then get it to contact the API, handling missing stuff properly. Then be able to view logs of a specific date if need be.
+    const [historyDepth, setHistoryDepth] = useState(1);
+    const dateArray = [];
+
+    for (let i = 0; i < historyDepth; i++) {
+        let tempDate = new Date();
+        tempDate.setDate(tempDate.getDate() - i);
+        let formattedDate =
+            tempDate.getDate() +
+            "-" +
+            (tempDate.getMonth() + 1).toString() +
+            "-" +
+            tempDate.getFullYear();
+        dateArray.push(formattedDate);
+    }
+
+    console.log(dateArray);
+
     return (
-        <div>
-            <LogHistoryViewer date="1-1-1111">
-                <LogHistoryElement
-                    username="Username"
-                    action="Action"
-                    time="12:00"
-                />
-                <LogHistoryElement
-                    username="Username"
-                    action="Action"
-                    time="12:01"
-                />
-                <LogHistoryElement
-                    username="Username"
-                    action="Action"
-                    time="12:02"
-                />
-                <LogHistoryElement
-                    username="Username"
-                    action="Action"
-                    time="12:03"
-                />
-            </LogHistoryViewer>
+        <div className="flex flex-col">
+            <input
+                type="number"
+                min="1"
+                max="10"
+                onChange={(e) => setHistoryDepth(e.target.value)}
+                className="bg-light text-2xl m-3 p-5 w-full"
+                placeholder="How many days back?"
+            />
+            {dateArray.map((date) => {
+                return <LogHistoryViewer date={date} />;
+            })}
         </div>
     );
 }
--- src/components/Routes/History/LogHistoryViewer.js
+++ src/components/Routes/History/LogHistoryViewer.js
@@ -1,8 +1,55 @@
-export default function LogHistoryViewer({ date, children }) {
+import { useEffect, useState } from "react";
+import axios from "axios";
+import LogHistoryElement from "./LogHistoryElement";
+
+export default function LogHistoryViewer({ date }) {
+    const [logArray, setLogArray] = useState([]);
+    useEffect(() => {
+        axios
+            .get("http://35.246.109.80:4000/history/" + date)
+            .then((response) => {
+                let tempLogArray = [];
+                let lines = response.data.split("\n").slice(0, -1);
+                for (let line of lines) {
+                    const usernameL = line.split(" ")[0];
+                    const actionL = line.split(" ")[2].slice(0, -1);
+                    const timeL = line.split(" ")[4];
+                    console.log(usernameL);
+                    console.log(actionL);
+                    console.log(timeL);
+
+                    tempLogArray.push({
+                        username: usernameL,
+                        action: actionL,
+                        time: timeL,
+                    });
+                }
+                setLogArray(tempLogArray);
+            })
+            .catch((err) => {
+                setLogArray([
+                    {
+                        username: "Hmmm. Nothing for today?",
+                        action: "",
+                        time: "",
+                    },
+                ]);
+            });
+    }, [date]);
     return (
         <div className="w-auto bg-light flex flex-col py-[0.5rem] my-[1rem] mx-[2rem] rounded-3xl">
             <h1 className="text-white text-3xl mx-[1rem]">{date}</h1>
-            <div className="text-white flex flex-col">{children}</div>
+            <div className="text-white flex flex-col">
+                {logArray.map((log) => {
+                    return (
+                        <LogHistoryElement
+                            username={log.username}
+                            action={log.action}
+                            time={log.time}
+                        />
+                    );
+                })}
+            </div>
         </div>
     );
 }
