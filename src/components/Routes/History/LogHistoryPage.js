0d5a6326fbc400031f0dd9603f93ad96dd33aba9
TinglyPants <chalkytangfastics@gmail.com>
2024-04-12 15:02:40 +0100

    Log History Page is now done.

--- src/components/Routes/History/LogHistoryPage.js
+++ src/components/Routes/History/LogHistoryPage.js
@@ -3,21 +3,26 @@ import LogHistoryViewer from "./LogHistoryViewer";
 
 export default function LogHistoryPage() {
     const [historyDepth, setHistoryDepth] = useState(1);
-    const dateArray = [];
+    const [customDate, setCustomDate] = useState("");
+    let dateArray = [];
 
-    for (let i = 0; i < historyDepth; i++) {
-        let tempDate = new Date();
-        tempDate.setDate(tempDate.getDate() - i);
-        let formattedDate =
-            tempDate.getDate() +
-            "-" +
-            (tempDate.getMonth() + 1).toString() +
-            "-" +
-            tempDate.getFullYear();
-        dateArray.push(formattedDate);
+    if (customDate === "") {
+        for (let i = 0; i < historyDepth; i++) {
+            let tempDate = new Date();
+            tempDate.setDate(tempDate.getDate() - i);
+            let formattedDate =
+                tempDate.getDate() +
+                "-" +
+                (tempDate.getMonth() + 1).toString() +
+                "-" +
+                tempDate.getFullYear();
+            dateArray.push(formattedDate);
+        }
+    } else {
+        dateArray = [customDate];
     }
 
-    console.log(dateArray);
+    console.log(customDate);
 
     return (
         <div className="flex flex-col">
@@ -26,8 +31,14 @@ export default function LogHistoryPage() {
                 min="1"
                 max="10"
                 onChange={(e) => setHistoryDepth(e.target.value)}
-                className="bg-light text-2xl m-3 p-5 w-full"
+                className="bg-light text-2xl m-3 p-5 w-auto"
                 placeholder="How many days back?"
+            />
+            <input
+                type="text"
+                onChange={(e) => setCustomDate(e.target.value)}
+                className="bg-light text-2xl m-3 p-5 w-auto"
+                placeholder="Any specific date?"
             />
             {dateArray.map((date) => {
                 return <LogHistoryViewer date={date} />;
